512c8575c7658eb1382ef6a683da7db59fbe9a3f
jona08n9 <99641671+jona08n9@users.noreply.github.com>
2024-01-05 10:37:37 +0100

    Made a test site, still not working right

---
+++ src/pages/admin/test.tsx
@@ -0,0 +1,66 @@
+import { LayoutAdmin } from '@/Layout_Admin';
+import { createClient } from '@supabase/supabase-js';
+
+import { supabase } from '../../../utils/supabaseClient';
+import { useRouter } from 'next/navigation';
+import { ChangeEvent, useEffect, useState } from 'react';
+import { log } from 'console';
+
+export const fetchSponsors = async () => {
+  const { data, error } = await supabase.storage.getBucket('sponsorer');
+  if (!data) {
+    console.log('error', error);
+  }
+  console.log('data', data);
+  console.log('Hej');
+};
+
+export default function Admin() {
+  const [file, setFile] = useState<string | null>(null);
+  const [fileDetails, setFileDetails] = useState<any>();
+
+  function handleInput(e: ChangeEvent<HTMLInputElement>) {
+    console.log(e.target.files);
+    if (e.target.files && e.target.files.length > 0) {
+      setFile(URL.createObjectURL(e.target.files[0]));
+      console.log(e.target.files[0]);
+      setFileDetails(e.target.files[0]);
+    }
+  }
+
+  async function uploadImage() {
+    // const { data, error } = await supabase.storage.from('sponsorer').upload(file?.name}, avatarFile);
+  }
+  // COMMENT OUT FROM HERE TO DISABLE LOGIN GUARD
+  const router = useRouter();
+
+  useEffect(() => {
+    getSession();
+  }, []);
+
+  async function getSession() {
+    const { data, error } = await supabase.auth.getSession();
+    if (data.session === null) {
+      router.push('/login');
+    }
+  }
+  // COMMENT OUT TO HERE TO DISABLE LOGIN GUARD
+
+  return (
+    <>
+      <LayoutAdmin>
+        <main className='spacer'>
+          <h1 className='mt-20' onClick={() => console.log(fileDetails)}>
+            {' '}
+            TEST site - For storage upload{' '}
+          </h1>
+          <div className=' flex flex-col gap-4 mt-6'>
+            <input type='file' onChange={handleInput} />
+            {file && <img src={file} alt='Uploaded file' width='150' height='auto' />}
+          </div>
+          <button onClick={() => uploadImage()}>Upload to Supabase </button>
+        </main>
+      </LayoutAdmin>
+    </>
+  );
+}
